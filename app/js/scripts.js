a402986379e16f0683ed1385af032941469bdd96
Vladislav Gryadko <zynga@ukr.net>
2020-05-26 14:25:56 +0300

    Добавил скрипт на кнопку закрытия меню в блоке header

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,1 +1,9 @@
+var header = document.querySelector(".header");
+var toggle = header.querySelector(".header__toggle");
+var navigation = header.querySelector(".main-nav");
 
+toggle.addEventListener("click", function(event) {
+    event.preventDefault();
+    navigation.classList.toggle("main-nav--closed");
+    toggle.classList.toggle("header__toggle--closed");
+});
