37c369f49e2a2b80ab31975e58510ba1a1597556
Vladislav Gryadko <zynga@ukr.net>
2020-05-27 21:27:40 +0300

    Добавил скрипт для слайдера в блоке examples

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,9 +1,19 @@
 var header = document.querySelector(".header");
 var toggle = header.querySelector(".header__toggle");
 var navigation = header.querySelector(".main-nav");
+var slider = document.querySelector(".expamples__slider");
+var imgSlider = slider.querySelectorAll(".examples__img");
+var sliderChecked = slider.querySelector(".examples__slide-checkbox");
 
 toggle.addEventListener("click", function(event) {
     event.preventDefault();
     navigation.classList.toggle("main-nav--closed");
     toggle.classList.toggle("header__toggle--closed");
+});
+
+sliderChecked.addEventListener("click", function(event) {
+    event.preventDefault();
+    sliderChecked.classList.toggle("examples__slide-checkbox--checked");
+    imgSlider[0].classList.toggle("examples__img--show");
+    imgSlider[1].classList.toggle("examples__img--show");
 });
